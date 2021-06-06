f1bd15034719e622fa06537c3039836fce90296b
Samuel Jarvis <samueladeyemi244@gmail.com>
2021-06-06 22:50:34 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0001B6',
+        secondary : {
+          100: '#FFB900',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Montserrat']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
